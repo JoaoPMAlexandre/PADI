e1485d714837a09dc1832629c5518b47b8ca8e33
JoaoPMAlexandre <joaopmalexandre@hotmail.com>
2015-04-08 16:19:27 +0100

    Joao Cenas

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PADIMapNoReduce
+{
+    public class UserInterface
+    {
+        Dictionary<int, String> Map;
+
+        public static void Main(string[] args)
+        {
+            UserInterface userinterface = new UserInterface();
+            string inputFile = Console.ReadLine();
+             userinterface.readFileAndParseIt(inputFile);
+            while (true)
+            {
+            }
+        }
+
+        public UserInterface()
+        {
+            Map = new Dictionary<int, String>();
+        }
+
+        public Dictionary<int, String> GetMap()
+        {
+            return Map;
+        }
+
+        public void readFileAndParseIt(String PathFile)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@PathFile);
+
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine("\t" + lines[i]);
+            }
+        }
+    }
+}
